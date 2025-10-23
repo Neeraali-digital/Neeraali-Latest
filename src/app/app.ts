acb935ad5ea399f066f658297ddd051746a5f2c1
Neeraali-digital <neeraalidigitalweb@gmail.com>
2025-10-23 11:43:22 +0530

    advertising slider

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'neeraali-digital';
+  slides: string[] = ['../assets/slide1.png', '../assets/slide2.png'];
+  currentSlide: number = 0;
+  private slideInterval: any;
+
+  ngOnInit() {
+    this.startSlideShow();
+  }
+
+  ngOnDestroy() {
+    if (this.slideInterval) {
+      clearInterval(this.slideInterval);
+    }
+  }
+
+  private startSlideShow() {
+    this.slideInterval = setInterval(() => {
+      this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+    }, 3000); // Change slide every 3 seconds
+  }
 }
